8897144f3531082ce1bea61dd00a237f2e82da41
Jay <151390193+jrmygts@users.noreply.github.com>
2025-07-27 18:54:10 -0500

    fix: only load dotenv in development

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,11 @@
 require_relative "boot"
 
 require "rails/all"
-Dotenv::Railtie.load
+
+if [ 'development', 'test' ].include?(ENV['RAILS_ENV'])
+  require 'dotenv'
+  Dotenv::Railtie.load
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
