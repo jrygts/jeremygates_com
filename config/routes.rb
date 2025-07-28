035542b7e7b86c4d1346cfd52a42241f6cc4e44d
Jay <151390193+jrmygts@users.noreply.github.com>
2025-07-27 21:42:28 -0500

    Set posts#index as root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "pages/home"
   get "home/index"
-  root "home#index"
+  root "posts#index"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
